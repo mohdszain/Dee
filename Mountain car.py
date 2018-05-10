89ff8cc53a7d4110ce339d60fa347b350f61d176
mohdszain <mohd.szain@gmail.com>
2018-05-09 19:31:30 -0500

    Add files via upload

---
+++ Mountain car.py
@@ -0,0 +1,11 @@
+import gym
+environment = gym.make('MountainCar-v0')
+for i in range(50):
+    test = environment.reset()
+    for t in range(100):
+        environment.render()
+        print(test)
+        action = environment.action_space.sample()
+        test, reward, done, info = environment.step(action)
+
+#Referred from: https://gym.openai.com/docs/
